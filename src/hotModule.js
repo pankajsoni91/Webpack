5450bb4eac0afb23e2c61ff823f0e3df892f5dae
Pankaj Soni <pankajsoni91@gmail.com>
2019-04-23 18:48:01 +0530

    updated the  code to commit to the changes

---
+++ src/hotModule.js
@@ -0,0 +1,3 @@
+export const hotModule = function() {
+  console.log("hot module");
+};
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,6 @@ module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   entry: "./src/index.js",
-  devServer: {
-    contentBase: "./dist"
-  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
