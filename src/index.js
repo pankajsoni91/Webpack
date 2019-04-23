376a5572e057e5dc772c1fc3f5c82728a01c6cdc
Pankaj Soni <pankajsoni91@gmail.com>
2019-04-23 18:17:29 +0530

    updated the code for the express and webpack

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 function component() {
   let element = document.createElement("div");
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack2"], " ");
+  element.innerHTML = _.join(["Hello", "webpack1"], " ");
   return element;
 }
 
